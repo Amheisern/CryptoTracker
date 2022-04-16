71b18695cc5840b88a175adfcc4e2f5108faf14b
lodashreload <icarusjapan@gmail.com>
2022-04-16 15:53:20 -0400

    set coin type and use state for coin value ... hard coded HTML

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 
+type CoinType = {
+  id: String | null
+  rank: String | null
+  // symbol: String | null
+  name: String | null
+  // supply: String | null
+  // maxSupply: String | null
+  // marketCapUsd: String | null
+  // volumeUsd24Hr: String | null
+  priceUsd: String | null
+  // changePercent24Hr: String | null
+  // vwap24Hr: String | null
+  // explorer: String | null
+}
 export function App() {
-  return <div>Hello, World </div>
+  const [coinsValue, setCoinsValue] = useState<CoinType[]>([])
+
+  async function getCoins() {
+    const response = await fetch('https://api.coincap.io/v2/assets')
+    if (response.ok) {
+      const data = await response.json()
+      console.log(data)
+    }
+    getCoins()
+  }
+  return (
+    <div>
+      <header>
+        <h1>Crypto Currency Price Tracker</h1>
+      </header>
+      <section>
+        <div className="tracker">bitcoin</div>
+        <div className="tracker">bitcoin</div>
+        <div className="tracker">bitcoin</div>
+        <div className="tracker">bitcoin</div>
+        <div className="tracker">bitcoin</div>
+      </section>
+      <footer>Created by Amheiser</footer>
+    </div>
+  )
 }
