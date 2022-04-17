f93512347a74586261680b63cf024f67ab0544ed
lodashreload <icarusjapan@gmail.com>
2022-04-17 15:16:26 -0400

    set time intervals done and working.  also loads currenctly on load

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ type CoinType = {
 }
 export function App() {
   const [coinsData, setCoinsData] = useState<CoinType[]>([])
+  const [cycles, setCycles] = useState<number>(0)
   console.log('coinsData', coinsData)
 
   function loadCoinsData() {
@@ -24,21 +25,37 @@ export function App() {
       if (response.ok) {
         const coins = await response.json()
         setCoinsData(coins.data)
+        //more function to be added here
         console.log('coins', coins)
       }
     }
     getCoins()
   }
   console.log('getCoins', loadCoinsData)
-  useEffect(loadCoinsData, [])
+
+  useEffect(function () {
+    const interval = setInterval(function () {
+      setCycles((cycles) => cycles + 1)
+    }, 10000)
+    return () => clearInterval(interval)
+  }, [])
+
+  useEffect(loadCoinsData, [cycles])
+
   return (
     <div>
       <header>
         <h1>Crypto Currency Price Tracker</h1>
+        <h2>{cycles}</h2>
       </header>
       <ul>
         {coinsData.map(function (coin) {
-          return <li key={coin.id}>{coin.name}</li>
+          return (
+            <li key={coin.id}>
+              {coin.name}
+              {coin.priceUsd}
+            </li>
+          )
         })}
       </ul>
       <footer>Created by Amheiser</footer>
