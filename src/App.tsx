427e26273964e06165463d5bf18d7946a5bc9b2b
lodashreload <icarusjapan@gmail.com>
2022-04-17 12:39:20 -0400

    can now load data from api

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,36 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 type CoinType = {
   id: number
   rank: String | null
-  // symbol: String | null
+  symbol: String | null
   name: String | null
-  // supply: String | null
-  // maxSupply: String | null
-  // marketCapUsd: String | null
-  // volumeUsd24Hr: String | null
+  supply: String | null
+  maxSupply: String | null
+  marketCapUsd: String | null
+  volumeUsd24Hr: String | null
   priceUsd: String | null
-  // changePercent24Hr: String | null
-  // vwap24Hr: String | null
-  // explorer: String | null
+  changePercent24Hr: String | null
+  vwap24Hr: String | null
+  explorer: String | null
 }
 export function App() {
-  const [coinsData, setCoinsData] = useState<CoinType[]>([
-    { id: 1, rank: '1', name: 'Bitcoin', priceUsd: '1' },
-    { id: 2, rank: '2', name: 'Ethereum', priceUsd: '2' },
-    { id: 3, rank: '3', name: 'Ripple', priceUsd: '3' },
-    { id: 4, rank: '4', name: 'Bitcoin Cash', priceUsd: '4' },
-  ])
+  const [coinsData, setCoinsData] = useState<CoinType[]>([])
   console.log('coinsData', coinsData)
 
-  async function getCoins() {
-    const response = await fetch('https://api.coincap.io/v2/assets')
-    if (response.ok) {
-      const data = await response.json()
-      console.log(data)
+  function loadCoinsData() {
+    async function getCoins() {
+      const response = await fetch('https://api.coincap.io/v2/assets')
+      if (response.ok) {
+        const coins = await response.json()
+        setCoinsData(coins.data)
+        console.log('coins', coins)
+      }
     }
     getCoins()
   }
+  console.log('getCoins', loadCoinsData)
+  useEffect(loadCoinsData, [])
   return (
     <div>
       <header>
@@ -38,13 +38,7 @@ export function App() {
       </header>
       <ul>
         {coinsData.map(function (coin) {
-          return (
-            <li key={coin.id}>
-              {coin.rank}
-              {coin.name}
-              {coin.priceUsd}
-            </li>
-          )
+          return <li key={coin.id}>{coin.name}</li>
         })}
       </ul>
       <footer>Created by Amheiser</footer>
