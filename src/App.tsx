0b8d0f99498be507b8c8ea1ecff42103fcbaafbf
lodashreload <icarusjapan@gmail.com>
2022-04-19 21:01:16 -0400

    mostly css fixes.  ii have abandoned adventure mode

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,17 @@ export function App() {
   //  const [cycleCoinData, setCycleCoinData] = useState([2,3,5,4,5,6,7,8])
   console.log('coinsData', coinsData)
 // console.log('cycleCoinData', cycleCoinData)
+  // const valueArray: number[] = []
 
+  // function compareValue(currencies: number[]){
+  // // valueArray.push(currency.priceUsd)
+  // const valueArray = currencies.map (currency => {
+  //   return currency.priceUsd
+  // })
+  
+  // console.log('valueArray', valueArray)
+  // // console.log('currency', currency)
+// }
   function loadCoinsData() {
     async function getCoins() {
       const response = await fetch('https://api.coincap.io/v2/assets')
@@ -29,6 +39,8 @@ export function App() {
         const {data} = await response.json()
         setCoinsData(data)
         console.log('coins', data)
+        // compareValue(data)
+        
         }
       }
       getCoins()
@@ -58,11 +70,11 @@ export function App() {
       </header>
       <ul>
         {coinsData
-          .filter((coin) => coin.rank <= 5)
+          .filter((coin) => coin.rank <= 25)
           .map(function (coin) {
             return (
               <li className="coins" key={coin.id}>
-                {coin.symbol}-{coin.rank}: {coin.name} ({Math.round(coin.priceUsd * 100) / 10000}$)
+                {coin.symbol}-{coin.rank}:  {coin.name}  ({Math.round(coin.priceUsd * 100) / 10000}$)
               </li>
             )
           })}
